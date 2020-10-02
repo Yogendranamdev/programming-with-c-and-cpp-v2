a366c8071346993e04db0b8693819fee8a95e034
Yogendranamdev <61061613+Yogendranamdev@users.noreply.github.com>
2020-10-02 22:46:59 +0530

    Update m1-t5-e3.c

--- m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
+++ m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
@@ -2,8 +2,8 @@
 
     int main() {
 
-        puts("I am Rahul.");
-        puts("I am 20 years old.");
+        printf("I am Yogendra");
+        printf("I am 23 years old.");
 
         return 0;
     }
